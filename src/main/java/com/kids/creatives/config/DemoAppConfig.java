1c907c770be81e7154c6cb57289acec35f422049
pfariso <siboiboip@gmail.com>
2019-03-18 14:08:04 +0200

    swagger Code documentation added
    it will help to read well all the API

--- src/main/java/com/kids/creatives/config/DemoAppConfig.java
+++ src/main/java/com/kids/creatives/config/DemoAppConfig.java
@@ -34,8 +34,8 @@ public class DemoAppConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		
-	//	registry.addResourceHandler("swagger-ui.html")
-	  //    .addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
 	 
 	    registry.addResourceHandler("/webjars/**")
 	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
